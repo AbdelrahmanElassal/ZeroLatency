31265454660decaaa2160dae4d462ba677a6a8bb
AbdelrahmanElassal <abdelrahmanelassal414@gmail.com>
2024-09-13 21:35:32 +0300

    ali morgan

--- streaming-server/sserver.js
+++ streaming-server/sserver.js
@@ -17,14 +17,6 @@ const server = app.listen(8080 , ()=>{
 });
 
 
-
-
-let iceCandidatesQueue = [];
-let isRemoteDescriptionSet = false;
-
-
-const socket = io('http://localhost:5000');
-
 const config = {
     iceServers: [
         { urls: "stun:stun.l.google.com:19302" }
@@ -32,36 +24,50 @@ const config = {
 };
 
 
-const peerConnection = new pkg.RTCPeerConnection(config);
-const dummyStream = new pkg.MediaStream({video : true , audio : true});
-const dummyTrack = dummyStream.getVideoTracks()[0] || dummyStream.getAudioTracks()[0];
-if (dummyTrack) {
-    peerConnection.addTrack(dummyTrack, dummyStream);
-}
+let iceCandidateMap = {};
+let isRemoteDescriptionMap = {};
+let peerConnectionMap = {};
 
-peerConnection.addEventListener('track', async (event) => {
-    const [remoteStream] = event.streams;
-});
 
-peerConnection.addEventListener('icecandidate', event => {
-    if (event.candidate) {
-        socket.emit('icecandidate', event.candidate);
-    }
-});
+const socket = io('http://localhost:5000');
 
-peerConnection.addEventListener('icegatheringstatechange', () => {
-    console.log('ICE Gathering State:', peerConnection.iceGatheringState);
-});
-
-peerConnection.addEventListener('connectionstatechange', event => {
-    console.log('peers are connected ',peerConnection.connectionState)
-});
-
-peerConnection.addEventListener('track', (stream)=>{
-    console.log("stream data here ",typeof stream);
-});
 
 socket.on('offer', async(offer)=>{
+
+    let id = offer.id;
+    iceCandidateMap[id] = [];
+    const peerConnection = new pkg.RTCPeerConnection(config);
+    const dummyStream = new pkg.MediaStream({video : true , audio : true});
+    const dummyTrack = dummyStream.getVideoTracks()[0] || dummyStream.getAudioTracks()[0];
+
+    peerConnectionMap[id] = peerConnection;
+    isRemoteDescriptionMap[id] = false;
+
+    console.log('peer   ', peerConnection);
+    peerConnection.addEventListener('track', async (event) => {
+        const [remoteStream] = event.streams;
+    });
+
+    peerConnection.addEventListener('icecandidate', event => {
+        if (event.candidate) {
+            socket.emit('icecandidate', event.candidate);
+        }
+    });
+
+    peerConnection.addEventListener('icegatheringstatechange', () => {
+        console.log('ICE Gathering State:', peerConnection.iceGatheringState);
+    });
+
+    peerConnection.addEventListener('connectionstatechange', event => {
+        console.log('peers are connected ',peerConnection.connectionState)
+    });
+
+    peerConnection.addEventListener('track', (stream)=>{
+        console.log("stream data here ",typeof stream);
+    });
+    if (dummyTrack) {
+        peerConnection.addTrack(dummyTrack, dummyStream);
+    }
     if(offer){
         const remoteDisc = new pkg.RTCSessionDescription(offer);
         await peerConnection.setRemoteDescription(remoteDisc);
@@ -69,28 +75,31 @@ socket.on('offer', async(offer)=>{
         const answer = await peerConnection.createAnswer();
         await peerConnection.setLocalDescription(answer);
         socket.emit('answer', answer);
-        isRemoteDescriptionSet = true;
-        iceCandidatesQueue.forEach(async candidate => {
-            await peerConnection.addIceCandidate(candidate);
-        });
-        iceCandidatesQueue = []; 
+        isRemoteDescriptionMap[id] = true;
+        if(iceCandidateMap[id]){
+            iceCandidateMap[id].forEach(async candidate => {
+                await peerConnectionMap[id].addIceCandidate(candidate);
+            });
+        }
+        iceCandidateMap[id] = [];
         console.log(socket.id , "sent my local disc");
     }
 })
 
 
 socket.on('icecandidate', async(candidate)=>{
+    let id = candidate.id;
     if (candidate) {
-        if (isRemoteDescriptionSet) {
+        if (isRemoteDescriptionMap[id]) {
             try {
-                await peerConnection.addIceCandidate(candidate);
+                await peerConnectionMap[id].addIceCandidate(candidate);
                 console.log("ICE candidate added");
             } catch (e) {
                 console.error('Error adding received ICE candidate', e);
             }
         } else {
             // Buffer ICE candidates until remote description is set
-            iceCandidatesQueue.push(candidate);
+            iceCandidateMap[id].push(candidate);
         }
     } else {
         console.log("Received an empty ICE candidate");
